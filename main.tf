fd8e6864218dff4f36404500b730b2f218252c59
“raj” <ec2-user@ip-10-0-4-88.ec2.internal>
2022-08-22 09:19:43 +0000

    updated files

--- main.tf
+++ main.tf
@@ -1,25 +1,19 @@
 terraform {
     backend "s3" {
-    bucket = "terraforminfracode"
+    bucket = "test-sanquest"
     key    = "terraform_Iac.tfstate"
     region = "us-east-1"
   }
 }
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
 
-  required_version = ">= 0.14.9"
+  required_version = ">= 0.12"
 }
 
 provider "aws" {
   profile = "default"
-  region  = "${var.region}"
+  region  = var.region
 }
 
 #module "aws_security_group" {
@@ -28,13 +22,13 @@ provider "aws" {
 #}
 
 resource "aws_instance" "controller_linux" {
-  ami                         = "${var.linux_ami_id}"
+  ami                         = var.linux_ami_id
   instance_type               = "t2.large"
   associate_public_ip_address = true
-  subnet_id = "${var.subnet}"
+  subnet_id = var.subnet
   key_name                    = "ssh-key"
   root_block_device {
-    volume_type           = "${var.volume_type}"
+    volume_type           = var.volume_type
     volume_size           = 60
     delete_on_termination = true
     encrypted = true
@@ -51,38 +45,37 @@ output "instance_ip_linux_controller" {
 
 resource "aws_key_pair" "ssh-key" {
   key_name   = "ssh-key"
-  public_key = "${var.ssh_key}"
+  public_key = var.ssh_key
 }
 
-
-resource "aws_security_group" "k8s-cluster-sg" {
-  name        = "k8s-cluster-sg"
+resource "aws_security_group" "devops-test1" {
+  name        = "devops-test1"
   description = "Allow sall traffic"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = "${var.cidr_blocks}"
+    cidr_blocks = var.cidr_blocks
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = "${var.cidr_blocks}"
+    cidr_blocks = var.cidr_blocks
   }
   tags = {
-    Name = "k8s-cluster-sg"
+    Name = "devops-test1"
   }
 }
 
 
 output "securitygroup_id" {
-  value = aws_security_group.k8s-cluster-sg.id
+  value = aws_security_group.devops-test1.id
 }
 
 resource "aws_network_interface_sg_attachment" "controller_sg_attachment" {
-  security_group_id    = aws_security_group.k8s-cluster-sg.id
+  security_group_id    = aws_security_group.devops-test1.id
   network_interface_id = aws_instance.controller_linux.primary_network_interface_id
 }
 
--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "linux_ami_id" {
     description = "ami id of the linux machine"
-    default = "ami-0598022403848c051"
+    default = "ami-052efd3df9dad4825"
 }
 variable "ssh_key" {
     description = "user ssh public key"
-    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZFuuu7wQCfVl3kHpnLPFfYTaSQ74IhEYV+Q2LXee6s0jtJsAo6BlZ7GUkPm6qa7Z+O2jXphe8VQsB5btMxv1Us5xS+AyIlPWFC1pRAsoJULmg8Tc7s97+l6BPzVwfDhFqIZkZpRnY7KYb438+/wA9ahdfeYb1BiptN/VUfGz12gmJhppDh5/MyyXUhwUrmLDSnsxr+VyLTfeffMhz2KXGF3UR3mjgPklIMpwMQJPoM6rEUJ4zpz+cVJLBRAQT0QXZMvjd+eNxg6bgWlnHYvK/1KdQDY6q/KaKMKJqHVNYRMpRgcBnfyHhiPP5ePzcB1aQgHJl9uwyTkwjcukvJZw7 cicd"
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCIVnbG9quGDQOkDDe1GdGhR7QI+sqf7+tpI1h1zq+xS4ZMQ1DeEkQVSXgmHYDjGPvinbapRU1987LDqKHdW+hcN6SqZ1i2C9t+R8tk/WIr5RejAcX2rqRoqqPIRAJUftcbGtJgTvoRVTgapemP4UCia3DGPCW1xp6B6/Gd5+HRF4rHr1C5vR6lSuo92uPreqd6fpN3jvq9n36Fdv3nmyu+6WmEmGYIIqSesI/7w+m/bOPIegoE2GZ/mAPCCDYUzp1r/C9R+JKzFZXLhCSxZsBWk+KSFmparlRdJW00UBYx+Ez57tcE9xiUv3y5hF55v9N572OugYBVOjurt0B1jTcp devops-training"
 }
 variable "vpc_id" {
     description = "vpc_id under which the resource group will create"
-    default = "vpc-06d9c1d3ecf203e11"
+    default = "vpc-082485d83a32d7336"
 }
 variable "subnet" {
     description = "subnet id of the vpc"
-    default = "	subnet-0c1622b82b3c369e3"
+    default = "	subnet-0b09b80c31a47edbf"
 }
 variable "cidr_blocks" {
     description = "subnet ip range in cidr block"
