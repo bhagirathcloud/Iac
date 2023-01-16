281760e302d299c75cd2c8e7d040f6b3fc77911c
bhagirathcloud <63989402+bhagirathcloud@users.noreply.github.com>
2023-01-15 22:33:34 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "linux_ami_id" {
     description = "ami id of the linux machine"
     default = "ami-052efd3df9dad4825"
 }
-variable "ssh_key" {
-    description = "user ssh public key"
-    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCIVnbG9quGDQOkDDe1GdGhR7QI+sqf7+tpI1h1zq+xS4ZMQ1DeEkQVSXgmHYDjGPvinbapRU1987LDqKHdW+hcN6SqZ1i2C9t+R8tk/WIr5RejAcX2rqRoqqPIRAJUftcbGtJgTvoRVTgapemP4UCia3DGPCW1xp6B6/Gd5+HRF4rHr1C5vR6lSuo92uPreqd6fpN3jvq9n36Fdv3nmyu+6WmEmGYIIqSesI/7w+m/bOPIegoE2GZ/mAPCCDYUzp1r/C9R+JKzFZXLhCSxZsBWk+KSFmparlRdJW00UBYx+Ez57tcE9xiUv3y5hF55v9N572OugYBVOjurt0B1jTcp devops-training"
-}
 variable "vpc_id" {
     description = "vpc_id under which the resource group will create"
     default = "vpc-082485d83a32d7336"
